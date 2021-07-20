c4f12a0613c640f2a1ca121dfb218de951c2fa78
jiaorongguo <903016522@qq.com>
2021-07-20 23:11:45 +0800

    第一次提交

---
+++ mall-products/src/main/java/com/jiaorongguo/mall/products/MallProductsApplication.java
@@ -0,0 +1,13 @@
+package com.jiaorongguo.mall.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductsApplication.class, args);
+    }
+
+}
---
+++ mall-store/src/main/java/com/jiaorongguo/mall/store/MallStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jiaorongguo.mall.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallStoreApplication.class, args);
+    }
+
+}
